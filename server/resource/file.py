45723064d3d2bd831ea19de780e0f30f1c49e623
Bluice Zhen <bluice.zhen@gmail.com>
2017-06-15 13:31:38 +0800

    remove qiniu

---
+++ server/resource/file.py
@@ -0,0 +1,12 @@
+from flask_restful import Resource
+
+
+class ResourceFile(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
--- server/resource/qiniu_upload_token.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Resource
-
-
-class ResourceQiniuUploadToken(Resource):
-    def get(self):
-        pass
