627b31c6f701674b1b9cebbb69dbb1d8cbc79223
Bluice Zhen <bluice.zhen@gmail.com>
2017-06-28 11:40:33 +0800

    1. Upload file to qiniu. 2. Run local test server.

--- server/resource/user_qiniu_upload_token_list.py
+++ server/resource/user_qiniu_upload_token_list.py
@@ -13,10 +13,11 @@ class ResourceUserQiNiuUploadTokenList(Resource):
     @auth_params_url(params_except=["file_name"])
     def get(self, user_uid: int) -> dict:
         seed = md5(str(time.time()).encode('utf-8')).hexdigest()
-        file_name = f"{user_uid}/{seed}/{g.params['file_name']}"
+        file_name = f"user/{user_uid}/{seed}/{g.params['file_name']}"
         token, url = ServiceQiNiu().get_upload_token(file_name)
 
         return {
+            "upload_key": file_name,
             "upload_token": token,
-            "url": url
+            "url": url,
         }
