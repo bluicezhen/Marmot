b3d60d4844ee1ed6636975bec16247f54be1e93f
Bluice Zhen <bluice.zhen@gmail.com>
2017-07-06 18:08:53 +0800

    ...

--- server/db/image.py
+++ server/db/image.py
@@ -1,1 +1,24 @@
-from
+from typing import Dict
+from uuid import uuid5, NAMESPACE_URL
+from sqlalchemy import Column, CHAR, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class TableImage(declarative_base()):
+    __tablename__ = "image"
+
+    uuid = Column(CHAR(36), primary_key=True)
+    title = Column(String(512))
+    url = Column(String(512))
+
+    def __init__(self, title: str, url: str):
+        self.uuid = str(uuid5(NAMESPACE_URL, url))
+        self.title = title
+        self.url = url
+
+    def to_dict(self) -> Dict[str, str]:
+        return {
+            "uuid": self.uuid,
+            "title": self.title,
+            "url": self.url
+        }
--- server/resource/user_image.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Resource
-
-
-class ResourceImage(Resource):
-    def get(self, image_uid: str) -> object:
-        pass
-
-    def delete(self, image_uid: str) -> object:
-        pass
