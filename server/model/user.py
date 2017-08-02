6311b95a5d836975f2064569def662a885c340ea
Bluice Zhen <bluice.zhen@gmail.com>
2017-08-02 20:02:26 +0800

    feat: ModelUser.find_one_by_username_password()

--- server/model/user.py
+++ server/model/user.py
@@ -1,4 +1,6 @@
 from server.db import TableUser
+from server.exc import ExceptionResponse
+from sqlalchemy.orm.exc import NoResultFound
 from ._model import Model
 
 
@@ -9,3 +11,11 @@ class ModelUser(Model):
         self.session.commit()
         self.session.refresh(user)
         return user
+
+    def find_one_by_username_password(self, username: str, password: str) -> TableUser:
+        try:
+            return self.session.query(TableUser) \
+                .filter(TableUser.username == username, TableUser.password == password) \
+                .one()
+        except NoResultFound:
+            raise ExceptionResponse(404, "Not Found")
