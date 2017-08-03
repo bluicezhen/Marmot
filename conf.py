8d2275022c630a5f9b88d714e2fbea5be2bc857b
Bluice Zhen <bluice.zhen@gmail.com>
2017-08-03 17:48:55 +0800

    feat: ModelUserToken.update_one_by_user_uuid_token_latest()

--- conf.py
+++ conf.py
@@ -4,6 +4,7 @@ debug = True
 def conf():
     if debug:
         return {
+            "token_validity": 60 * 60,
             "qiniu": {
                 "access_key": "t7eyEfX6fXJOmG3b_-6B8lgLXwmShOLcmcIeYMSQ",
                 "secret_key": "3m5rOjW3P3859hralq9pAlHrQNV58S2DSIOZR8yY",
--- server/model/user_token.py
+++ server/model/user_token.py
@@ -1,3 +1,5 @@
+from conf import conf
+from datetime import datetime, timedelta
 from server.db import TableUserToken
 from ._model import Model
 
@@ -7,3 +9,11 @@ class ModelUserToken(Model):
         user_token = TableUserToken(user_uuid)
         self.session.add(user_token)
         return user_token
+
+    def update_one_by_user_uuid_token_latest(self, user_uuid: str, token: str) -> int:
+        now = datetime.now()
+        token_validity = conf()["token_validity"]
+        return self.session.query(TableUserToken) \
+            .filter(TableUserToken.user_uuid == user_uuid, TableUserToken.token == token,
+                    TableUserToken.time_update > now - timedelta(seconds=token_validity)) \
+            .update({TableUserToken.time_update: now})
